5e09c64a463fe4606a7fd5b9f8679740f0c6b747
Dell <rabiulrafi.cse.just@gmail.com>
2019-03-01 13:23:43 +0600

    Activity Added

---
+++ app/src/main/java/com/example/dell/justadmissionaid/SeatPlan.java
@@ -0,0 +1,13 @@
+package com.example.dell.justadmissionaid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SeatPlan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seat_plan);
+    }
+}
